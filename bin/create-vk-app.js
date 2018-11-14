4aced212ac27648f6fb4a23e2a003f8d796306e0
kuhel <vorontsov.gleb@gmail.com>
2018-11-14 13:45:18 +0300

    Support older node verison, engines in package.json

--- bin/create-vk-app.js
+++ bin/create-vk-app.js
@@ -2,13 +2,25 @@
 
 const fs = require('fs-extra');
 const path = require('path');
-const https = require('https');
 const { exec } = require('child_process');
 
 const packageJson = require('../package.json');
 
 const scripts = `"start": "cross-env PORT=10888 react-scripts start",
 "build": "react-scripts build"`;
+
+// Polyfill
+if (!Object.entries) {
+	Object.entries = function( obj ){
+	  var ownProps = Object.keys( obj ),
+		  i = ownProps.length,
+		  resArray = new Array(i); // preallocate the Array
+	  while (i--)
+		resArray[i] = [ownProps[i], obj[ownProps[i]]];
+	  
+	  return resArray;
+	};
+  }
 
 // Parse and prepare string to install dependencies
 const getDeps = deps =>
@@ -47,7 +59,7 @@ exec(
 
 		for (let i = 0; i < filesToCopy.length; i += 1) {
 			fs.createReadStream(path.join(__dirname, `../${filesToCopy[i]}`))
-				.pipe(fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`));
+			.pipe(fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`));
 		}
 
 
@@ -89,23 +101,23 @@ package-lock.json
 
 				console.log('⏱  Copying VK App source files..');
 				fs.copy(path.join(__dirname, '../public'), `${process.argv[2]}/public`)
-					.then(() => console.log(`🖼  Assets directory and file copied`))
-					.catch(err => console.error(err));
+				.then(() => console.log(`🖼  Assets directory and file copied`))
+				.catch(err => console.error(err));
 
 				fs.copy(path.join(__dirname, '../src'), `${process.argv[2]}/src`)
-					.then(() => {
-						// TODO Config feature will be enabled later
-						// if (process.argv[3] !== undefined) {
-						// 	const configFile = `${process.argv[2]}/src/config.js`;
-						// 	const data = `export default {\n\tappId: ${process.argv[3]},\n};\n`;
-						// 	fs.writeFile(configFile, data, (err) => {
-						// 		if (err) throw err;
-						// 	});
-						// }
-						console.log(`✌️  VK App Boilerplate is ready to start in ${process.argv[2]} folder. \n🧐  Check README.MD for brief instructrions.\n💻  Happy Coding!`)
-					})
-					.catch(err => console.error(err));
-			},
+				.then(() => {
+					// TODO Config feature will be enabled later
+					// if (process.argv[3] !== undefined) {
+					// 	const configFile = `${process.argv[2]}/src/config.js`;
+					// 	const data = `export default {\n\tappId: ${process.argv[3]},\n};\n`;
+					// 	fs.writeFile(configFile, data, (err) => {
+					// 		if (err) throw err;
+					// 	});
+					// }
+					console.log(`✌️  VK App Boilerplate is ready to start in ${process.argv[2]} folder. \n🧐  Check README.MD for brief instructrions.\n💻  Happy Coding!`)
+				})
+				.catch(err => console.error(err));
+			}
 		);
-	},
+	}
 );
