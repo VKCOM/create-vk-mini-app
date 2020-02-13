8992274ee8b01129dbbc1fa5e9e9a32b7605cffa
kuhel <vorontsov.gleb@gmail.com>
2020-02-13 11:24:26 +0300

    VKUI 3

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { platform, IOS } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
@@ -15,9 +15,9 @@ const osName = platform();
 const Persik = props => (
 	<Panel id={props.id}>
 		<PanelHeader
-			left={<HeaderButton onClick={props.go} data-to="home">
+			left={<PanelHeaderButton onClick={props.go} data-to="home">
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</HeaderButton>}
+			</PanelHeaderButton>}
 		>
 			Persik
 		</PanelHeader>
