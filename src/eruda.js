97b04b9b2e283713525b4c8f511135215f2dd0a6
nocell <zuev.oleg21@gmail.com>
2020-03-23 09:44:02 +0700

    fix eruda import

---
+++ src/eruda.js
@@ -0,0 +1,9 @@
+import * as eruda from 'eruda';
+import * as erudaCode from 'eruda-code';
+import * as erudaDom from 'eruda-dom';
+
+eruda.init();
+eruda.add(erudaCode);
+eruda.add(erudaDom);
+
+export default eruda
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import 'core-js/features/map';
-import 'core-js/features/set';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import bridge from '@vkontakte/vk-bridge';
-import App from './App';
-import * as eruda from 'eruda';
-import * as erudaCode from 'eruda-code';
-import * as erudaDom from 'eruda-dom';
+import "core-js/features/map";
+import "core-js/features/set";
+import React from "react";
+import ReactDOM from "react-dom";
+import bridge from "@vkontakte/vk-bridge";
+import App from "./App";
 
 // Init VK  Mini App
-bridge.send('VKWebAppInit');
+bridge.send("VKWebAppInit");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-if (process.env.NODE_ENV === 'development') {
-	eruda.init();
-	eruda.add(erudaCode);
-	eruda.add(erudaDom);
+ReactDOM.render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV === "development") {
+  import("./eruda").then(eruda => {}); //runtime download
 }
