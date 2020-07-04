413cd5071dc037ddc6b75262c0f6cdcc9eccbde8
Ilya <45998835+ilyapishchulin@users.noreply.github.com>
2020-07-04 23:21:53 +0300

    Use PanelHeaderBack for very beautiful code

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -3,21 +3,15 @@ import PropTypes from 'prop-types';
 import { platform, IOS } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
-import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
+import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
 
 import persik from '../img/persik.png';
 import './Persik.css';
 
-const osName = platform();
-
 const Persik = props => (
 	<Panel id={props.id}>
 		<PanelHeader
-			left={<PanelHeaderButton onClick={props.go} data-to="home">
-				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</PanelHeaderButton>}
+			left={<PanelHeaderBack onClick={props.go} data-to="home"/>}
 		>
 			Persik
 		</PanelHeader>
