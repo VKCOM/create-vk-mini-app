63e4e82e92bbd8329fce9d376a1a97cae2605df4
Ilya <45998835+ilyapishchulin@users.noreply.github.com>
2020-07-04 23:22:46 +0300

    Delete import platform

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { platform, IOS } from '@vkontakte/vkui';
+
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
