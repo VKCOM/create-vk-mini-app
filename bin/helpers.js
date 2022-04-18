c225cfcd349207fab24df6eff4cd2f3eebf58d26
Denis Tereshchenko <d.tereshchenko@corp.vk.com>
2022-04-18 18:13:51 +0300

    Fixed readme file

--- bin/helpers.js
+++ bin/helpers.js
@@ -1,10 +1,11 @@
 const fs = require('fs-extra');
 
 function modifyPackageJson(path, cb) {
-  const packageJson = require(path);
-  const modifiedPackageJson = cb(file);
+  const rawData = fs.readFileSync(path);
+  const parsedJson = JSON.parse(rawData);
+  const modifiedPackageJson = cb(parsedJson);
 
-  fs.writeFileSync(path, JSON.stringify(modifyPackageJson, 0, 4));
+  fs.writeFileSync(path, JSON.stringify(modifiedPackageJson, 0, 4));
 }
 
 module.exports = {
