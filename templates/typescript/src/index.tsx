d5e817fd7c9ced24f1fedd5f9c7db0765580a9a1
Inomdzhon Mirdzhamolov <i.mirdzhamolov@vk.team>
2023-09-18 14:57:58 +0300

    feat: add vk-bridge intergration with vkui (#111)
    Добавил интеграцию **VK Bridge** с **VKUI** в шаблоны `templates/javascript` и `template/typescript`.
    
    По инструкции **VKUI** [Интеграция с VK Mini Apps](https://vkcom.github.io/VKUI/5.8.0-beta.0/#/integrations-vk-mini-apps).

--- templates/typescript/src/index.tsx
+++ templates/typescript/src/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bridge from '@vkontakte/vk-bridge';
+
+import { ConfigProvider, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
+import vkBridge, { parseURLSearchParamsForGetLaunchParams } from '@vkontakte/vk-bridge';
+import { useAdaptivity, useAppearance, useInsets } from '@vkontakte/vk-bridge-react';
+import { transformVKBridgeAdaptivity } from './transformers/transformVKBridgeAdaptivity';
 import App from './App';
 
 // Init VK  Mini App
-bridge.send('VKWebAppInit');
+vkBridge.send('VKWebAppInit');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const InjectBridge = () => {
+  const vkBridgeAppearance = useAppearance() || undefined; // You can replace undefined with your default value
+  const vkBridgeInsets = useInsets() || undefined; // You can replace undefined with your default value
+  const vkBridgeAdaptivityProps = transformVKBridgeAdaptivity(useAdaptivity());
+  const { vk_platform } = parseURLSearchParamsForGetLaunchParams(window.location.search); // [optional] platform can be provided by URL (see https://dev.vk.com/mini-apps/development/launch-params#vk_platform)
+
+  return (
+    <ConfigProvider
+      appearance={vkBridgeAppearance}
+      platform={vk_platform === 'desktop_web' ? 'vkcom' : undefined}
+      isWebView={vkBridge.isWebView()}
+      hasCustomPanelHeaderAfter={true} // Reserve right side of PanelHeader for VK Mini Apps control buttons
+    >
+      <AdaptivityProvider {...vkBridgeAdaptivityProps}>
+        <AppRoot mode="full" safeAreaInsets={vkBridgeInsets}>
+          <App />
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
+  );
+};
+
+ReactDOM.render(<InjectBridge />, document.getElementById('root'));
 
 if (process.env.NODE_ENV === 'development') {
   import('./eruda').then(({ default: eruda }) => {}); //runtime download
