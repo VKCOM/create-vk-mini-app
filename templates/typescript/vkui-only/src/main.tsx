13c2ae432acd43fa6926bacac255adc911981deb
v.gritsenko <v.gritsenko@vk.team>
2024-02-21 18:58:00 +0300

    Потерял пробел

--- templates/typescript/vkui-only/src/main.tsx
+++ templates/typescript/vkui-only/src/main.tsx
@@ -1,4 +1,4 @@
-import { createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { AdaptivityProvider, AppRoot, ConfigProvider } from '@vkontakte/vkui';
 
